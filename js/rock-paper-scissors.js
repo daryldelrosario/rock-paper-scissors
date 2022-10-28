5165e89393caf345eb5acd87891a240059551d15
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-10-27 20:57:46 -0400

    Create function playround() with proper parameters

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,21 +3,87 @@ function getComputerChoice() {
     
     switch (computerChoice) {
         case 1:
-            console.log("Rock");
+            return("Rock");
             break;
         case 2:
-            console.log("Paper");
+            return("Paper");
             break;
         case 3:
-            console.log("Scissors");
+            return("Scissors");
             break;
         default: 
-            console.log("Experiencing Technical Difficulties");
+            return("Experiencing Technical Difficulties");
     }
 }
 
-function loopPCChoice(no) {
-    for(let i = 0; i <= no; i++ ) {
-        getComputerChoice();
+function getPlayerChoice() {
+    let playerChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch (playerChoice) {
+        case 1:
+            return("Rock");
+            break;
+        case 2: 
+            return("Paper");
+            break;
+        case 3:
+            return("Scissors");
+            break;
+        default:
+            return("Experiencing Technical Difficulties");
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    let message = "";
+
+    if(playerSelection.toLowerCase() === "rock") {
+        switch (computerSelection) {
+            case "Rock":
+                return message = "Both of you chose Rock, it's a Tie!";
+                break;
+            case "Paper":
+                return message = "You Lose! Paper beats Rock.";
+                break;
+            case "Scissors":
+                return message = "You Win! Rock beats Scissors.";
+                break;
+            default:
+                return("Experiencing Technical Difficulties");
+        }
+    } else if(playerSelection.toLowerCase() === "paper") {
+        switch (computerSelection) {
+            case "Rock":
+                return message = "You Win! Paper beats Rock.";
+                break;
+            case "Paper":
+                return message = "Both of you chose Paper, it's a Tie!";
+                break;
+            case "Scissors":
+                return message = "You Lose! Scissors beats Paper.";
+                break;
+            default:
+                return("Experiencing Techincal Difficulties");
+        }
+    } else {
+        switch (computerSelection) {
+            case "Rock":
+                return message = "You Lose! Rock beats Scissors.";
+                break;
+            case "Paper":
+                return message = "You Win! Scissors beats Paper.";
+                break;
+            case "Scissors":
+                return message = "Both of you chose Scissors, it's a Tie!";
+                break;
+            default:
+                return("Experiencing Techical Difficulties");
+        }
     }
 }
