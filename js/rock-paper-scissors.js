5f8f9bc868fed83776d15a217be61032603feb86
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-10-27 20:23:15 -0400

    Create function getComputerChoice()

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    
+    switch (computerChoice) {
+        case 1:
+            console.log("Rock");
+            break;
+        case 2:
+            console.log("Paper");
+            break;
+        case 3:
+            console.log("Scissors");
+            break;
+        default: 
+            console.log("Experiencing Technical Difficulties");
+    }
+}
+
+function loopPCChoice(no) {
+    for(let i = 0; i <= no; i++ ) {
+        getComputerChoice();
+    }
+}
